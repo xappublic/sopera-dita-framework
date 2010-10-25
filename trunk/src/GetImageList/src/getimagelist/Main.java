66a71a685b63f2082c13aaf1ec361adbd260d697
Pavel.S.Vasilchenko@gmail.com <Pavel.S.Vasilchenko@gmail.com@eafe4085-930b-5871-4687-119b0ae92ee2>
2010-10-25 17:56:08 +0000

    - update program for getting information about images

--- trunk/src/GetImageList/src/getimagelist/Main.java
+++ trunk/src/GetImageList/src/getimagelist/Main.java
@@ -35,60 +35,88 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws SAXException, ParserConfigurationException, XPathExpressionException, IOException {
+        int flag = 1;
         String mapFilePath = "";
         if(args.length == 0) { System.out.println("add arg with path to xml file"); return; }
-        else mapFilePath = args[0];
+        else
+        {
+            mapFilePath = args[0];
+            if(mapFilePath.equals("begin")) flag = 0;
+            if(mapFilePath.equals("end")) flag = 2;
+        }
         //mapFilePath = "C:\\bin\\sopera-dita-framework\\in\\sopera_intalio_ui_overview.xml";
 
-        File mapFile = new File(mapFilePath);
+        File mapFile = null;
+        NodeList nodes = null;
         
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        factory.setValidating(false);
-        factory.setNamespaceAware(true); // never forget this!
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        builder.setEntityResolver(new EntityResolver() { 
-            @Override
-            public InputSource resolveEntity(String publicId, String systemId)
-                    throws SAXException, IOException {
-                System.out.println("Ignoring " + publicId + ", " + systemId);
-                return new InputSource(new StringReader(""));
-            }
-        });
-        org.w3c.dom.Document doc = builder.parse(mapFilePath);
-        XPathFactory xPathFactory = XPathFactory.newInstance();
-        XPath xpath = xPathFactory.newXPath();
-        XPathExpression expr = xpath.compile("//image");
-        Object result = expr.evaluate(doc, XPathConstants.NODESET);
-        NodeList nodes = (NodeList) result;
-
-
-
-        String res = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\r\n<images>\r\n";
-        OutputStreamWriter f = new OutputStreamWriter(new FileOutputStream("images.xml"), "UTF8");
-        try {
-            for (int i = 0; i < nodes.getLength(); i++) {
-                res += "<image href=\"";
-                String href = nodes.item(i).getAttributes().getNamedItem("href").getNodeValue();
-                res += href;
-                res += "\"";
-                //////////////////////////////////
-                System.out.println(mapFile.getParent() +  mapFile.separator + href);
-                File inFile = new File(mapFile.getParent() +  mapFile.separator + href);
-                Image image = (Image) ImageIO.read(inFile);
-                res += " width=\"" + image.getWidth(null) + "\"";
-                res += " height=\"" + image.getHeight(null) + "\"";
-                //////////////////////////////////                
-                res += " />\r\n";            
-            }
-            res += "</images>";
+        if(flag==1)
+        {
+            mapFile = new File(mapFilePath);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setValidating(false);
+            factory.setNamespaceAware(true); // never forget this!
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            builder.setEntityResolver(new EntityResolver() {
+                @Override
+                public InputSource resolveEntity(String publicId, String systemId)
+                        throws SAXException, IOException {
+                    System.out.println("Ignoring " + publicId + ", " + systemId);
+                    return new InputSource(new StringReader(""));
+                }
+            });
+            org.w3c.dom.Document doc = builder.parse(mapFilePath);
+            XPathFactory xPathFactory = XPathFactory.newInstance();
+            XPath xpath = xPathFactory.newXPath();
+            XPathExpression expr = xpath.compile("//image");
+            Object result = expr.evaluate(doc, XPathConstants.NODESET);
+            nodes = (NodeList) result;
+        }
+        String res = "";
+        if(flag == 0)
+        {
+            res = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\r\n<images>\r\n";
+            OutputStreamWriter f = new OutputStreamWriter(new FileOutputStream("images.xml", false), "UTF8");
             char[] buflast = new char[res.length()];
             res.getChars(0, buflast.length, buflast, 0);
             f.write(buflast, 0, buflast.length);
             f.flush();
         }
-        catch (IOException e) {
-          try {f.close();} catch (Exception e1) {};
-          return;
-         }
+        if(flag == 1)
+        {
+            OutputStreamWriter f = new OutputStreamWriter(new FileOutputStream("images.xml", true), "UTF8");
+            try {
+                for (int i = 0; i < nodes.getLength(); i++) {
+                    res += "<image href=\"";
+                    String href = nodes.item(i).getAttributes().getNamedItem("href").getNodeValue().replace('/', '\\');
+                    res += href;
+                    res += "\"";
+                    //////////////////////////////////
+                    System.out.println(mapFile.getParent() +  "\\" + href);
+                    File inFile = new File(mapFile.getParent() +  "\\" + href);
+                    Image image = (Image) ImageIO.read(inFile);
+                    res += " width=\"" + image.getWidth(null) + "\"";
+                    res += " height=\"" + image.getHeight(null) + "\"";
+                    //////////////////////////////////
+                    res += " />\r\n";
+                }
+                char[] buflast = new char[res.length()];
+                res.getChars(0, buflast.length, buflast, 0);
+                f.write(buflast, 0, buflast.length);
+                f.flush();
+            }
+            catch (IOException e) {
+              try {f.close();} catch (Exception e1) {};
+              return;
+             }
+        }
+        if(flag == 2)
+        {
+            res = "</images>";
+            OutputStreamWriter f = new OutputStreamWriter(new FileOutputStream("images.xml", true), "UTF8");
+            char[] buflast = new char[res.length()];
+            res.getChars(0, buflast.length, buflast, 0);
+            f.write(buflast, 0, buflast.length);
+            f.flush();
+        }
     }
 }
