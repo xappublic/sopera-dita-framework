525dde6df8105e6e06456632b0caaebd67335fa4
Pavel.S.Vasilchenko@gmail.com <Pavel.S.Vasilchenko@gmail.com@eafe4085-930b-5871-4687-119b0ae92ee2>
2010-11-08 08:28:05 +0000

    - update sources "get images list" application

---
+++ trunk/src/GetImageList/src/FilesList.java
@@ -0,0 +1,11 @@
+public class FilesList {
+
+    public String rootCatalogpath;
+    public String filePath;
+    
+    public FilesList(String dir, String file)
+    {        
+        rootCatalogpath = dir;
+        filePath = file;
+    }    
+}
